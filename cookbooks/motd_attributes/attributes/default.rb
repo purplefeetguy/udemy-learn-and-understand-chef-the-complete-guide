e43c4f7cb0359d2f892579b50dd25a2a9cab50a2
purplefeetguy <purplefeetguy@hotmail.com>
2019-01-26 11:55:04 -0600

    using override precedence in attributes

--- cookbooks/motd_attributes/attributes/default.rb
+++ cookbooks/motd_attributes/attributes/default.rb
@@ -1,8 +1,14 @@
 # set attribute precedence; no need for node reference as all attributes in an attribute file
 # are assumed to be node attributes
-default['motd_attributes']['company'] = "0'Reilly"
-# try to set attribute to override OHAI automatic attribute; this fails due to precedence
-default['ipaddress'] = '1.1.1.1'
+# change the attribute precedence from default to "override" to prevent changes from
+# other sources
+override['motd_attributes']['company'] = "0'Reilly"
+# even if we set another attribute to try to overwrite
+# last one in can't change the and attribute set to "override"
+default['motd_attributes']['company'] = "No'Reilly"
+# "override" still can't change an "automatic" attribute as "automatic" is the highest
+# precedence
+override['ipaddress'] = '1.1.1.1'
 default['motd_attributes']['java_bin'] = 'usr/local/bin/java'
 # Composed Attributes are bad
 default['motd_attributes']['tomcat_home'] = '/usr/local/tomcat6'
